215190a051b077aa7f8adb1e4ac508d3f4363293
DanilSord <kincharov99@gmail.com>
2021-07-29 13:01:03 +0700

    getbjectithroperties function to common

--- src/core/common.js
+++ src/core/common.js
@@ -8,5 +8,14 @@ const getRandomHashSHA1 = () => {
 
 const getMD5Hash = ( buffer ) => crypto.createHash( 'md5' ).update( buffer ).digest( 'hex' );
 
+const getObjectWithProperties = ( props, object ) => {
+  const result = {};
+  for ( const prop of props ) {
+    if ( Object.prototype.hasOwnProperty.call( object, prop ) ) {
+      result[ prop ] = object[ prop ];
+    }
+  }
+  return result;
+};
 
-module.exports = { getRandomHashSHA1, getMD5Hash };
+module.exports = { getRandomHashSHA1, getMD5Hash, getObjectWithProperties };
